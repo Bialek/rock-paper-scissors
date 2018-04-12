7bf73496e2e5be1fdf1fc29df6c8b4fc310f1a7b
Rafał Białek <bialekrafal94@gmail.com>
2018-04-12 21:02:12 +0200

    add 3 buttons & eventlisteners

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
-var newGameBtn = document.getElementById("js-newGameButton");
+var newGameBtn = document.getElementById("newGameButton");
+var pickRock = document.getElementById("playerPickRock");
+var pickPaper = document.getElementById("playerPickPaper");
+var pickScissors = document.getElementById("playerPickScissors");
 
 newGameBtn.addEventListener("click", newGame);
+pickRock.addEventListener("click", function() { playerPick("rock") } );
+pickPaper.addEventListener("click", function() { playerPick("paper") } );
+pickScissors.addEventListener("click", function() { playerPick("scissors") } );
